99ce9ccdf8bdda68401f3be1de9a18d50134b0b3
hhost-madsen <hhost-madsen@users.noreply.github.com>
2016-08-29 04:04:50 -0400

    Add files via upload

---
+++ drawing.py
@@ -0,0 +1,16 @@
+import swampy.TurtleWorld
+
+world.clear()
+world = TurtleWorld()
+bob = Turtle()
+print bob
+
+wait_for_user()
+
+fd(bob, 100)
+lt(bob)
+fd(bob, 100)
+lt(bob)
+fd(bob, 100)
+lt(bob)
+fd(bob, 100)
